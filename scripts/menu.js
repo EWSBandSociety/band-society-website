3e33da862a80a069767b327da2ff7cde7e7946fb
Ethan Bayer <studioethanb@gmail.com>
2025-09-23 09:47:39 -0500

    Added menu toggling and button icon switching

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,3 +3,13 @@ Menu button JS script
 This file is licensed under the MIT license.
 Copyright (C) 2025 Ethan Bayer
 */
+
+const nav = document.getElementById("menu-nav");
+const menuIcon = document.getElementById("menu-icon");
+const xIcon = document.getElementById("x-icon");
+
+function openCloseMenu() {
+  nav.classList.toggle("opened-menu");
+  menuIcon.classList.toggle("no-display");
+  xIcon.classList.toggle("no-display");
+}
